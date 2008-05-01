2c2900b96262d1ec8de25a683a53e471bcb70df0
Jim Fulton <jim@zope.com>
2008-05-01 20:06:40 +0000

    Added zpl and minor rearrangement.

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
+##############################################################################
+#
+# Copyright (c) Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+name = 'zc.zdaemonrecipe'
+version = '0.1'
+
 import os
 from setuptools import setup, find_packages
 
@@ -23,10 +39,9 @@ long_description=(
 
 open('doc.txt', 'w').write(long_description)
 
-name = 'zc.zdaemonrecipe'
 setup(
     name = name,
-    version = '0.1',
+    version = version,
     author = 'Jim Fulton',
     author_email = 'jim@zope.com',
     description = 'ZC Buildout recipe for zdaemon scripts',
