2c8e7fe8d4acaff2c72ad41918c94325dfae1da7
Fred Drake <fred@zope.com>
2013-04-01 11:38:10 -0400

    back to development

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ tests_require = ['zope.testing']
 
 setup(
     name = name,
-    version='0.3.1',
+    version='0.3.2.dev0',
     author = 'Jim Fulton',
     author_email = 'jim@zope.com',
     description = 'ZC Buildout recipe for zdaemon scripts',
